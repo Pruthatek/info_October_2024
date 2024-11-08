cee84c5f8c11d90fdf2bab94de16b413991535db
MuskanPruthatek <muskan@pruthatek.com>
2024-11-08 11:16:15 +0530

    changes in settings.py

--- backend/blogpost/settings.py
+++ backend/blogpost/settings.py
@@ -1,11 +1,25 @@
 import os
 from pathlib import Path
+from corsheaders.defaults import default_headers
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-i%)s)7z+&wv6++)bi=5!%d$dk)$qfb1hpzwmun06-n#!b$)%ct'
 DEBUG = True  # Set to True during development
 ALLOWED_HOSTS = ['*']
+
+# Add this to settings.py temporarily for debugging
+CORS_ALLOW_HEADERS = list(default_headers) + [
+    'Access-Control-Allow-Origin',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    "https://167.71.224.61",
+    "http://pruthatek.info",
+    "https://pruthatek.info",
+    "http://localhost:5173"
+]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
@@ -15,8 +29,20 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8001",
     "http://localhost:5174",
     "https://urban-space-telegram-w46qj6xwjrvf9w6x-8000.app.github.dev",
-    "https://167.71.224.61"
+    "https://167.71.224.61",
+    "http://pruthatek.info",
+    "https://pruthatek.info"
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://pruthatek.info",
+    "https://pruthatek.info"
+]
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -34,10 +60,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
